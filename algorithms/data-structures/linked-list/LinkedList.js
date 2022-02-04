b9f8d486744440cc631b16e51fb04eafe6bf9d10
billowstao <22577367+billowstao@users.noreply.github.com>
2022-02-04 23:28:13 +0800

    feat(algorithm): data structure - linked list

--- algorithms/data-structures/linked-list/LinkedList.js
+++ algorithms/data-structures/linked-list/LinkedList.js
@@ -85,6 +85,7 @@ export default class LinkedList {
    * 删除
    *
    * @param {*} value - 删除的节点
+   * @returns 删除的节点
    */
   delete(value) {
     if (!this.head) {
@@ -122,6 +123,149 @@ export default class LinkedList {
   }
 
   /**
-   * TODO: 搜索
+   * 搜索
+   *
+   * @param value - 检索的值
+   * @param findCallback - 检索回调
+   * @returns 搜索的节点
    */
+  find(value, findCallback) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (!currentNode) {
+      // 如果指定查找回调，通过回调查找节点
+      if (typeof findCallback === "function" && findCallback(currentNode)) {
+        return currentNode;
+      }
+
+      if (this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  /**
+   * 删除尾节点
+   *
+   * @returns 删除的尾节点
+   */
+  deleteTail() {
+    const deletedTail = this.tail;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    // 如果链表存在很多节点
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+
+    return deletedTail;
+  }
+
+  /**
+   * 删除头节点
+   *
+   * @returns 删除的头节点
+   */
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deleteHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deleteHead;
+  }
+
+  /**
+   * 将数组转换为链表
+   *
+   * @param values - 数组数值
+   */
+  fromArray(values) {
+    values.forEach((value) => this.append(value));
+
+    return this;
+  }
+
+  /**
+   * 转换为数组
+   *
+   * @returns 数组
+   */
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  /**
+   * 输出字符串
+   *
+   * @param callback - 转换回调函数
+   */
+  toString(callback) {
+    return this.toArray().map((node) => node.toString(callback).toString());
+  }
+
+  /**
+   * 反转链表
+   *
+   * @returns 反转的链表
+   */
+  reverse() {
+    let currNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currNode) {
+      nextNode = currNode.next;
+
+      // 修改当前节点链接指向前一个节点
+      currNode.next = prevNode;
+
+      // 向前移动 `prevNode`, `currNode`
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+
+    // 重新设置 `head`, `tail`
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
+  }
 }
