5590c055b86d38ebfa85674f07d56d7db580f16a
billowstao <22577367+billowstao@users.noreply.github.com>
2022-02-10 23:29:27 +0800

    feat(algorithm): data structure - doubly linked list - finished

--- algorithms/data-structures/doubly-linked-list/DoublyLinkedList.js
+++ algorithms/data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -102,4 +102,153 @@ export default class DoublyLinkedList {
 
     return deletedNode;
   }
+
+  /**
+   * 查找
+   *
+   * @param value - 查找的值
+   * @param callback - 查找回调
+   * @returns 查找到的节点
+   */
+  find(value, callback) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      // 如果指定回调函数，使用回调函数查找
+      if (typeof callback === "function" && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      if (this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  /**
+   * 删除尾节点
+   *
+   * @returns 删除的节点
+   */
+  deleteTail() {
+    if (!this.tail) {
+      return null;
+    }
+
+    if (this.head === this.tail) {
+      const deletedTail = this.tail;
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    const deletedTail = this.tail;
+
+    this.tail = this.tail.previous;
+    this.tail.next = null;
+
+    return deletedTail;
+  }
+
+  /**
+   * 删除头节点
+   *
+   * @returns 删除的头节点
+   */
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+      this.head.previous = null;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
+  }
+
+  /**
+   * 转换为数组
+   *
+   * @returns 链表数组
+   */
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  /**
+   * 从数组转换为链表
+   *
+   * @param values - 数组数据
+   * @returns 双向链表
+   */
+  fromArray(values) {
+    values.forEach((value) => this.append(value));
+
+    return this;
+  }
+
+  /**
+   * 转换为字符串
+   *
+   * @param callback - 转换回调
+   * @returns 链表字符串
+   */
+  toString(callback) {
+    return this.toArray()
+      .map((node) =>
+        node.toString(typeof callback === "function" && callback(node))
+      )
+      .toString();
+  }
+
+  /**
+   * 反转链表
+   *
+   * @returns 双向链表
+   */
+  reverse() {
+    let currNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currNode) {
+      nextNode = currNode.next;
+      prevNode = currNode.previous;
+
+      currNode.next = prevNode;
+      currNode.previous = nextNode;
+
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
+  }
 }
